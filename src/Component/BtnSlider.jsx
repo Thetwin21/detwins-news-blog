9b5aaee3bb859980544946908ce3d53fac9ba4c4
Ogodo Sylvester <Sylvesterogodo@gmail.com>
2022-06-09 05:09:41 -0700

    first commit

--- src/Component/BtnSlider.jsx
+++ src/Component/BtnSlider.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const BtnSlider = ({direction, mouseSlide }) => {
     return (
        <button className={direction === 'right' ? 'btn btn_right' : 'btn btn_left'} onClick={mouseSlide}>
-           <img src={direction === 'right' ? './arrow right.png' : './arrow left.png' } />
+           <img src={direction === 'right' ? './arrow right.png' : './arrow left.png' } alt='icon img' />
        </button>
     )
 }
