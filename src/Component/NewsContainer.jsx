18a36f9e6289ad984cc4bc9702328cbbe4981761
Ogodo Sylvester <Sylvesterogodo@gmail.com>
2022-06-09 05:21:35 -0700

    first commit

--- src/Component/NewsContainer.jsx
+++ src/Component/NewsContainer.jsx
@@ -14,16 +14,16 @@ const NewsContainer = () => {
 		'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
 	}
 };
-    const getarticle = () => {
+    const getArticle = () =>{
 
-fetch(`https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=+${articles}&pageNumber=1&pageSize=10&autoCorrect=true`, options)
+     fetch(`https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=+${articles}&pageNumber=1&pageSize=10&autoCorrect=true`, options)
 	.then(response => response.json())
 	.then(data => setContainer(data.value))
 	.catch(err => console.error(err));
     }
 
     useEffect(() => {
-        getarticle()
+        getArticle()
     }, [articles])
 
     // ========Input data section ===========
